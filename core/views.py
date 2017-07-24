b72f01496f03ee03a71f3b074c6d2ed809774728
murmurise <zhy47@126.com>
2017-07-24 15:26:50 +0800

    update core app

---
+++ core/views.py
@@ -0,0 +1,11 @@
+# core/views.py
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+
+# Create your views here.
+@login_required
+def home(request):
+    return render(request, 'core/lookup.html')
